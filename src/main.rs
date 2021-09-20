66165619320594e8fcdb5252f388236e9bbace7a
cc-morning <90960002+cc-morning@users.noreply.github.com>
2021-09-20 14:38:25 +0800

    Support for image sharing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
+use arboard::{Clipboard, ImageData};
 use clap::{App, Arg};
-use clipboard::{ClipboardContext, ClipboardProvider};
 use deadpool_redis::{Config, Connection, Pool};
 use futures_util::stream::StreamExt;
 use mimalloc::MiMalloc;
@@ -9,19 +9,17 @@ use rayon::prelude::*;
 use redis::cmd;
 use rsa::{PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
 use std::{
+    borrow::Cow,
     collections::HashSet,
     error::Error,
     sync::{
         atomic::{AtomicBool, Ordering},
-        Arc, Mutex,
+        Arc,
     },
     time::Duration,
 };
 use tokio::{
-    sync::{
-        mpsc::{self, Sender},
-        RwLock,
-    },
+    sync::{mpsc, RwLock},
     time,
 };
 
@@ -32,6 +30,12 @@ static GLOBAL: MiMalloc = MiMalloc;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    ctrlc::set_handler(move || {
+        if !EXIT.load(Ordering::Relaxed) {
+            EXIT.swap(true, Ordering::Relaxed);
+        }
+    })?;
+
     let matches = App::new("share-clip")
         .version("0.1.0")
         .author("morning")
@@ -60,40 +64,46 @@ async fn main() -> Result<()> {
                 .takes_value(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("fast")
+                .short("f")
+                .long("fast")
+                .value_name("B")
+                .takes_value(true)
+                .required(false),
+        )
         .get_matches();
 
-    ctrlc::set_handler(move || {
-        if !EXIT.load(Ordering::Relaxed) {
-            EXIT.swap(true, Ordering::Relaxed);
-        }
-    })?;
+    let url = matches.value_of("url").unwrap();
+    let code = matches.value_of("code").unwrap();
+    let name = matches.value_of("name").unwrap();
 
-    let url = matches.value_of("url").unwrap_or_default();
     let pool = Arc::new(Config::from_url(url).create_pool()?);
+    let pre_md5 = Arc::new(RwLock::new(String::new()));
+    let (publish_key, cache_key, match_key) = (
+        format!("sub_{}_{}", code, name),
+        format!("key:{}:{}", code, name),
+        format!("key:{}:*", code),
+    );
 
-    let code = matches.value_of("code").unwrap_or_default();
-    let name = matches.value_of("name").unwrap_or_default();
-
-    let priv_cache_key = format!("key:{}:{}", code, name);
-    let priv_key_exists = {
+    let encrypt_key_exists = {
         cmd("EXISTS")
-            .arg(priv_cache_key.clone())
+            .arg(cache_key.clone())
             .query_async::<_, i32>(&mut pool.get().await?)
             .await?
     };
-
-    let pub_key = {
-        let priv_key = if priv_key_exists == 0 {
-            let priv_key = RsaPrivateKey::new(&mut OsRng, 2048)?;
+    let encrypt_key = {
+        let priv_key = if encrypt_key_exists == 0 {
+            let priv_key = RsaPrivateKey::new(&mut OsRng, 1024)?;
             cmd("SET")
-                .arg(priv_cache_key.clone())
+                .arg(cache_key.clone())
                 .arg(bincode::serialize(&priv_key)?)
                 .query_async::<_, ()>(&mut pool.get().await?)
                 .await?;
             priv_key
         } else {
             let bytes = cmd("GET")
-                .arg(priv_cache_key.clone())
+                .arg(cache_key.clone())
                 .query_async::<_, Vec<u8>>(&mut pool.get().await?)
                 .await?;
             bincode::deserialize::<RsaPrivateKey>(&bytes)?
@@ -102,79 +112,88 @@ async fn main() -> Result<()> {
         RsaPublicKey::from(&priv_key)
     };
 
-    let (clip_tx, mut clip_rx) = mpsc::channel::<String>(1024);
-    let pre_text = Arc::new(RwLock::new(String::new()));
-
-    let pre_text_clone = pre_text.clone();
-    tokio::spawn(async move {
-        let _ = monitor_clip(pre_text_clone, clip_tx).await;
+    let moniter_future = monitor_clip(pool.clone(), pre_md5.clone(), publish_key, encrypt_key);
+    tokio::spawn(async {
+        let _ = moniter_future.await;
     });
 
-    let publish_key = format!("sub_{}_{}", code, name);
-    let pool_clone = pool.clone();
-    tokio::spawn(async move {
-        loop {
-            if let Some(i) = clip_rx.recv().await {
-                let bytes = i
-                    .as_bytes()
-                    .par_chunks(96)
-                    .filter_map(|data| {
-                        pub_key
-                            .encrypt(&mut OsRng, PaddingScheme::PKCS1v15Encrypt, data)
-                            .ok()
-                    })
-                    .map(|data| base64::encode(data))
-                    .collect::<Vec<String>>()
-                    .join(",");
-
-                cmd("PUBLISH")
-                    .arg(&publish_key)
-                    .arg(bytes)
-                    .query_async::<_, ()>(&mut pool_clone.get().await.unwrap())
-                    .await
-                    .unwrap();
-            }
-        }
-    });
-
-    let _ = sub_clip(
-        pool.clone(),
-        pre_text.clone(),
-        priv_cache_key.clone(),
-        format!("key:{}:*", code),
-    )
-    .await;
-
+    let _ = sub_clip(pool.clone(), pre_md5.clone(), cache_key.clone(), match_key).await;
     cmd("DEL")
-        .arg(priv_cache_key.clone())
+        .arg(cache_key.clone())
         .query_async::<_, ()>(&mut pool.get().await?)
         .await?;
 
     Ok(())
 }
 
-async fn monitor_clip(pre_text: Arc<RwLock<String>>, sender: Sender<String>) -> Result<()> {
-    let mut clip_ctx: ClipboardContext = ClipboardProvider::new().expect("创建剪切板对象失败!");
+async fn monitor_clip(
+    pool: Arc<Pool>,
+    pre_md5: Arc<RwLock<String>>,
+    publish_key: String,
+    encrypt_key: RsaPublicKey,
+) -> Result<()> {
+    let mut clip = Clipboard::new()?;
 
     loop {
-        let content = {
-            let _write = pre_text.write().await;
-            let content = match clip_ctx.get_contents() {
-                Ok(v) => v,
-                Err(_) => continue,
-            };
-            content
+        let mut write = pre_md5.write().await;
+
+        let (text, text_md5) = match clip.get_text() {
+            Ok(text) => {
+                let md5 = format!("{:x}", md5::compute(&text));
+                (Some(text), md5)
+            }
+            _ => (None, "".to_string()),
+        };
+        let (image, image_md5) = match clip.get_image() {
+            Ok(image) => {
+                let md5 = format!("{:x}", md5::compute(&image.bytes));
+                (Some((image.bytes.to_vec(), image.height, image.width)), md5)
+            }
+            _ => (None, "".to_string()),
         };
 
-        if !pre_text.read().await.eq(&content) {
-            match sender.send(content.clone()).await {
-                Ok(_) => {
-                    let mut write = pre_text.write().await;
-                    write.clear();
-                    write.push_str(&content);
-                }
-                Err(_) => {}
-            }
+        let (mut data, bytes, md5) = if text.is_some() {
+            let data = String::from("0-");
+            let bytes = text.unwrap().as_bytes().to_vec();
+            (data, bytes, text_md5)
+        } else if image.is_some() {
+            let (bytes, height, width) = image.unwrap();
+            let data = format!("1{},{}-", height, width);
+            (data, bytes, image_md5)
+        } else {
+            time::sleep(Duration::from_secs(1)).await;
+            continue;
+        };
+
+        if !write.eq(&md5) {
+            write.clone_from(&md5);
+            write.shrink_to_fit();
+            drop(write);
+
+            data.push_str(
+                bytes
+                    .par_chunks(64)
+                    .enumerate()
+                    .map(|(index, data)| {
+                        if index == 0 {
+                            encrypt_key
+                                .encrypt(&mut OsRng, PaddingScheme::PKCS1v15Encrypt, data)
+                                .unwrap()
+                        } else {
+                            Vec::from(data)
+                        }
+                    })
+                    .map(|data| base64::encode(data))
+                    .collect::<Vec<String>>()
+                    .join(",")
+                    .as_str(),
+            );
+
+            cmd("PUBLISH")
+                .arg(&publish_key)
+                .arg(data)
+                .query_async::<_, ()>(&mut pool.get().await?)
+                .await?;
         }
 
         time::sleep(Duration::from_secs(1)).await;
@@ -183,21 +202,20 @@ async fn monitor_clip(pre_text: Arc<RwLock<String>>, sender: Sender<String>) -> 
 
 async fn sub_clip(
     pool: Arc<Pool>,
-    pre_text: Arc<RwLock<String>>,
-    priv_cache_key: String,
-    all_code_key: String,
+    pre_md5: Arc<RwLock<String>>,
+    cache_key: String,
+    match_key: String,
 ) -> Result<()> {
     let (sub_tx, mut sub_rx) = mpsc::channel::<String>(1024);
-    let mut subed = HashSet::new();
+    let mut subscribed = HashSet::new();
 
     let pool_clone = pool.clone();
     tokio::spawn(async move {
         loop {
-            let pool_clone = pool_clone.clone();
-            let pre_text_clone = pre_text.clone();
             if let Some(key) = sub_rx.recv().await {
-                tokio::spawn(async move {
-                    let _ = dev_sub_clip(pool_clone, key, pre_text_clone).await;
+                let dev_sub_future = sub_clip_on_device(pool_clone.clone(), pre_md5.clone(), key);
+                tokio::spawn(async {
+                    let _ = dev_sub_future.await;
                 });
             }
         }
@@ -209,12 +227,13 @@ async fn sub_clip(
         }
 
         let keys = cmd("KEYS")
-            .arg(&all_code_key)
+            .arg(&match_key)
             .query_async::<_, Vec<String>>(&mut pool.get().await?)
             .await?;
+
         for key in keys {
-            if !subed.contains(&key) && !key.eq(&priv_cache_key) {
-                subed.insert(key.clone());
+            if !subscribed.contains(&key) && !key.eq(&cache_key) {
+                subscribed.insert(key.clone());
                 sub_tx.send(key).await?;
             }
         }
@@ -225,54 +244,93 @@ async fn sub_clip(
     Ok(())
 }
 
-async fn dev_sub_clip(
+async fn sub_clip_on_device(
     pool: Arc<Pool>,
+    pre_md5: Arc<RwLock<String>>,
     key: String,
-    pre_text: Arc<RwLock<String>>,
 ) -> Result<(), Box<dyn Error>> {
-    let mut clip_ctx: ClipboardContext = ClipboardProvider::new()?;
+    let mut clip = Clipboard::new()?;
 
-    let priv_key = {
+    let decrypt_key = {
         let bytes = cmd("GET")
             .arg(key.clone())
             .query_async::<_, Vec<u8>>(&mut pool.get().await?)
             .await?;
         bincode::deserialize::<RsaPrivateKey>(&bytes)?
     };
-    let (subscribe_key, code, name) = {
+    let subscribe_key = {
         let key_array = key.split(":").collect::<Vec<&str>>();
         let (code, name) = (*key_array.get(1).unwrap(), *key_array.get(2).unwrap());
-        (format!("sub_{}_{}", code, name), code, name)
+        format!("sub_{}_{}", code, name)
     };
 
     let mut pubsub = Connection::take(pool.get().await?).into_pubsub();
     pubsub.subscribe(subscribe_key).await?;
-    println!("Sub -> {}-{}", code, name);
 
     loop {
         if let Some(msg) = pubsub.on_message().next().await {
             let payload = msg.get_payload::<String>()?;
 
-            let bytes = Mutex::new(vec![]);
-            payload
-                .split(",")
-                .par_bridge()
-                .map(|cipher| {
-                    let data = base64::decode(cipher).unwrap();
-                    let data = priv_key
-                        .decrypt(PaddingScheme::PKCS1v15Encrypt, &data)
-                        .unwrap();
-                    data
-                })
-                .for_each(|data| {
-                    data.iter().for_each(|i| bytes.lock().unwrap().push(*i));
-                });
-            let text = String::from_utf8_lossy(&bytes.lock().unwrap()).to_string();
+            let marker: &i32 = &payload[0..1].parse()?;
+            let payload = &payload[1..];
 
-            let mut write = pre_text.write().await;
-            write.clear();
-            write.push_str(text.as_str());
-            clip_ctx.set_contents(text)?;
+            let mut split = payload.split("-");
+            let prop = split.next();
+            let data = match split.next() {
+                Some(v) => v,
+                None => continue,
+            };
+
+            let bytes = std::sync::Mutex::new(vec![]);
+            data.split(",")
+                .collect::<Vec<&str>>()
+                .par_iter()
+                .enumerate()
+                .map(|(index, data)| {
+                    let data = base64::decode(data).unwrap();
+                    if index == 0 {
+                        decrypt_key
+                            .decrypt(PaddingScheme::PKCS1v15Encrypt, &data)
+                            .unwrap()
+                    } else {
+                        data
+                    }
+                })
+                .collect::<Vec<Vec<u8>>>()
+                .iter()
+                .for_each(|data| {
+                    data.iter()
+                        .for_each(|data| bytes.lock().unwrap().push(*data));
+                });
+
+            let mut write = pre_md5.write().await;
+            let md5 = match marker {
+                0 => {
+                    let text = String::from_utf8_lossy(&bytes.lock().unwrap()).to_string();
+                    let md5 = format!("{:x}", md5::compute(&text));
+                    clip.set_text(text)?;
+
+                    md5
+                }
+                1 => {
+                    let mut prop_split = prop.unwrap().split(",");
+                    let height: usize = prop_split.next().unwrap().parse()?;
+                    let width: usize = prop_split.next().unwrap().parse()?;
+
+                    let vec = bytes.lock().unwrap().clone();
+                    let image = ImageData {
+                        width: width,
+                        height: height,
+                        bytes: Cow::from(&vec),
+                    };
+                    clip.set_image(image)?;
+
+                    format!("{:x}", md5::compute(&vec))
+                }
+                _ => continue,
+            };
+            write.clone_from(&md5);
+            write.shrink_to_fit();
         }
     }
 }
